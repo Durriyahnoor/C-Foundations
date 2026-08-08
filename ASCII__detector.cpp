3cf8f68822df5c46ba72b3e4a91cfd8cccddd681
Maryam Akhtar <durriyahnoor@gmail.com>
2026-08-08 07:35:01 +0500

    ASCII value detector program in C++

---
+++ ASCII__detector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    char my_letter= 'A';
+
+    cout<<"=================================================================" << endl;
+    cout<<"                   *** ASCII Value detector ***                  " << endl;
+    cout<<"=================================================================" << endl;
+    cout<<"  My letter is: "<< my_letter << endl;
+    cout<<"  The ASCII value of my letter is: "<< int(my_letter) << endl;
+    cout<<"-----------------------------------------------------------------" << endl;
+    return 0;
+}
